0c37080a805509dfc98e7a61ffc24de55bd61c0d
sgillespie <sgillespie@714ce8b5-1e2d-0410-af88-bba6d96a00db>
2007-04-02 10:46:38 +0000

    initial commit

---
+++ trunk/lib/builtin.py
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------
+#
+# cl_builtins.py - Built in functions
+# ------------------------------------------------------------------
+
+def add(obj1, obj2):
+    return obj1 + obj2
+
+def subtract(obj1, obj2):
+    return obj1 - obj2
+
+def multiply(obj1, obj2):
+    return obj1 * obj2
+
+def divide(obj1, obj2):
+    return obj1 / obj2
