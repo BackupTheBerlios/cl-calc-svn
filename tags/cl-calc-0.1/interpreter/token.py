fda2c68f15a97ff512644d209c300d6f612dbc71
sgillespie <sgillespie@714ce8b5-1e2d-0410-af88-bba6d96a00db>
2007-04-02 10:53:16 +0000

    Tagging release 0.1

---
+++ tags/cl-calc-0.1/interpreter/token.py
@@ -0,0 +1,55 @@
+# ------------------------------------------------------------------------
+# cl-lex.py
+# 
+# Tokenizer for the calculator evaluator.
+# ------------------------------------------------------------------------
+
+import ply.lex
+
+
+
+# tokens
+tokens = (
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+    'LBRACK',
+    'RBRACK',
+    'LBRACE',
+    'RBRACE',
+)
+
+
+# token definitions
+t_NUMBER = r'[0-9]*(\.)?[0-9]+'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'\/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACK = r'\['
+t_RBRACK = r'\]'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+
+
+# ignore these chars
+t_ignore = ' \t'
+
+
+
+# error definition
+def t_error(t):
+    print "Illegal character '%s'" %(t.value[0])
+    t.lexer.skip(1)
+    #Since PLY doesn't let me return my own types, I will have to
+    #push my error onto the stack and then retrieve it
+    #error.CLError(message='Illegal character "%s"' %(t.value[0]))
+
+# set up lexer
+ply.lex.lex()
